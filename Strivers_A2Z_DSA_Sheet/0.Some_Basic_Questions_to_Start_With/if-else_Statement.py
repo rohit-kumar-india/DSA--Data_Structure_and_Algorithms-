7a2fbdb08eaab5b521194172791121b3c1de792e
rohit-kumar-india <rohit.rk2146@gmail.com>
2024-02-03 23:18:09 +0530

    Learned If Else

---
+++ Strivers_A2Z_DSA_Sheet/0.Some_Basic_Questions_to_Start_With/if-else_Statement.py
@@ -0,0 +1,58 @@
+''' #Python
+CN Link: https://www.codingninjas.com/studio/problems/if-else-decision-making_8357235?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf
+Problem statement: Programming languages have some conditional / decision-making statements that execute when some specific condition is fulfilled.
+
+If-else is one of the ways to implement them.
+
+You are given two numbers 'a' and 'b'.
+
+Compare the numbers and print the relation.
+
+Print “smaller”, “greater” or “equal” when ‘a’ is smaller than ‘b’, greater than ‘b’, or equal to ‘b’ respectively.
+
+Example :
+Input: ‘a’ = 5 and ‘b’ = 3
+
+Output: greater
+
+Explanation: Since ‘a’ (= 5) is greater than ‘b’ (= 3), we are printing “greater”.
+Detailed explanation ( Input/output format, Notes, Images )
+Sample Input 1:
+5 3
+
+Sample Output 1:
+greater
+
+Explanation of sample input 1 :
+Since ‘a’ (= 5) is greater than ‘b’ (= 3), we are printing “greater”.
+
+Sample Input 2:
+2 2
+
+Sample Output 2:
+equal
+
+Explanation of sample input 2 :
+Since ‘a’ (= 2) is equal to ‘b’ (= 2), we are printing “equal”.
+
+Expected time complexity :
+The expected time complexity is O(1).
+
+Constraints :
+-10 ^ 5 <= ‘a’ <= 10 ^ 5
+-10 ^ 5 <= ‘b’ <= 10 ^ 5
+
+Time limit: 1 second
+'''
+
+def compareIfElse(a: int, b: int):
+    if a>b:
+        return "greater"
+    elif a<b:
+        return "smaller"
+    else:
+        return "equal"
+    
+a=list(map(int, input().strip().split()))
+
+print(compareIfElse(a[0],a[1]))
