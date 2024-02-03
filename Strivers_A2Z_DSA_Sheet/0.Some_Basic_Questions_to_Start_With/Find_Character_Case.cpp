15ffada10590a36f0ccfba51d2ace25e81e35fe3
rohit-kumar-india <rohit.rk2146@gmail.com>
2024-02-03 17:44:11 +0530

    git setup

---
+++ Strivers_A2Z_DSA_Sheet/0.Some_Basic_Questions_to_Start_With/Find_Character_Case.cpp
@@ -0,0 +1,65 @@
+/*
+Problem statement: Write a program that takes a character as input and prints 1, 0, or -1 according to the following rules.
+
+1, if the character is an uppercase alphabet (A - Z).
+0, if the character is a lowercase alphabet (a - z).
+-1, if the character is not an alphabet.
+
+
+Example:
+Input: The character is 'a'.
+
+Output: 0
+
+Explanation: The input character is lowercase, so our answer is 0.
+Detailed explanation ( Input/output format, Notes, Images )
+
+Sample Input 1 :
+v
+
+Sample Output 1 :
+0
+
+Explanation of Sample Input 1:
+The input character is lowercase, so our answer is 0.
+
+Sample Input 2 :
+V
+
+Sample Output 2 :
+1
+
+Explanation of Sample Input 2:
+The input character is uppercase, so our answer is 1.
+
+Sample Input 3 :
+#
+
+Sample Output 3 :
+-1
+
+Explanation of Sample Input 3:
+The input character is not an alphabet, so our answer is -1.
+
+Constraints :
+The input can be any single character.
+*/
+
+
+
+#include<iostream>
+using namespace std;
+
+int main() {
+	
+	char c;
+	cin >> c;
+
+	if(int(c)>=65 and int(c)<=90)
+		cout << 1;
+	else if(int(c)>=97 and int(c)<=122)
+		cout << 0;
+	else
+		cout << -1;
+	
+}
